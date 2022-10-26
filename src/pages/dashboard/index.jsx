b09846d35c412c185fc589049e2a41c6ed68bf8a
Gabriela <gabrielamedeiros998@gmail.com>
2022-10-26 13:52:39 -0400

    layout dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import { Badge, Col, Row } from "react-bootstrap";
 import "../dashboard/index.style.css";
 import MenuOptionsComponents from "../shared/menu.options";
+import { Chart } from "react-google-charts";
 
 export default function DashboardCompnent() {
+
+  const data = [
+    ["description", "total"],
+    ["BD COM ERRO", 10],
+    ["BD INCONSISTENTE", 3],
+    ["BD OK", 70]
+  ];
+
+  const options = {
+    slices: {0: {color: '#DC3545'}, 1: {color: '#FFC107'}, 2:{color:'#198754'}},
+    legend: {position: 'none'}
+  };
+    
+  
   return (
     <Row className="h-100 w-100">
       <Col className="p-0" style={{ maxWidth: "250px" }}>
@@ -30,22 +45,33 @@ export default function DashboardCompnent() {
               BD COM ERRO
             </Badge>
             <Badge className={"badge__dashboard"} bg={"warning"} text="white">
-              <span>10</span>
+              <span>3</span>
               BD INCONSISTENTE
             </Badge>
             <Badge className={"badge__dashboard"} bg={"success"} text="white">
-              <span>10</span>
+              <span>70</span>
               BD OK
             </Badge>
 
             <Badge className={"badge__dashboard"} bg={"secondary"} text="white">
-              <span>10</span>
+              <span>5</span>
               SERVIDOR
             </Badge>
             <Badge className={"badge__dashboard"} bg={"secondary"} text="white">
-              <span>10</span>
+              <span>100</span>
               TOTAL BD
             </Badge>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="col-12">
+            <Chart
+              chartType="PieChart"
+              data={data}
+              options={options}
+              width="100%"
+              height="400px"
+            />
           </Col>
         </Row>
       </Col>
